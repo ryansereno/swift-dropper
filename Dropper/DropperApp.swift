1019ff30fba374703b3093596ccfbf9a1fd6dc8c
ryansereno <ryansereno@protonmail.com>
2025-01-04 19:14:19 -0500

    Initial Commit

---
+++ Dropper/DropperApp.swift
@@ -0,0 +1,17 @@
+//
+//  DropperApp.swift
+//  Dropper
+//
+//  Created by Ryan on 1/4/25.
+//
+
+import SwiftUI
+
+@main
+struct DropperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
