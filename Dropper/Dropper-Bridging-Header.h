736bf6236da7d24cd6417eb885423bce8aabcf24
ryansereno <ryansereno@protonmail.com>
2025-01-05 06:24:04 -0500

    added bridging header

---
+++ Dropper/Dropper-Bridging-Header.h
@@ -0,0 +1,13 @@
+//
+//  Dropper-Bridging-Header.h
+//  Dropper
+//
+//  Created by Ryan on 1/5/25.
+//
+
+#ifndef Dropper_Bridging_Header_h
+#define Dropper_Bridging_Header_h
+
+#import "PaintMixerBridge.h"
+
+#endif /* Dropper_Bridging_Header_h */
